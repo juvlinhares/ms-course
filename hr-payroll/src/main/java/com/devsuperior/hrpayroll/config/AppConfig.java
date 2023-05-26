462ff2ee2dbb79b6d24f10e50225b046e7ee13c5
Juvencio Linhares <juvlinhares@gmail.com>
2023-05-26 00:17:43 -0300

    Feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/config/AppConfig.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-//vai instanciar meu restTemplate
+//configura e cria uma instancia de rest Template:
 @Configuration
 public class AppConfig {
-	
+
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
 }
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
@@ -1,38 +1,28 @@
 package com.devsuperior.hrpayroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	// colocar numa variavel o valor do hr-worker.host
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
-	// injeção de RestTemplate:
+	// injeção de dependencia do workerfeignClient:
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerfeignClient;
 
-	// create a payment:
+	// metodo que vai instanciar o pagamento:
+
 	public Payment getPayment(Long workerId, Integer days) {
-		// criar mapa de parametros:
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
-		
-		//fazer a requisição restTemplate
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
 
-		return new Payment(worker.getName(),worker.getDailyInCome(), days);
+		// montagem do path do worker:
+		Worker worker = workerfeignClient.findById(workerId).getBody();
 
+		// instancia do pagamento com as propriedades do worker:
+		return new Payment(worker.getName(), worker.getDailyInCome(), days);
 	}
+
 }
