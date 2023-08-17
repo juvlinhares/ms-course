2d14463d0c9b179658a36ae1646ddc22da4a3c32
Juvencio Linhares <juvlinhares@gmail.com>
2023-08-17 09:16:21 -0300

    user feign client

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feigns.UserFeignClient;
+
+@Service
+public class UserService {
+
+	Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+	if(user == null) {
+		logger.error("Email not found: " + email);
+	}
+	logger.info("Email found: " +email);
+	return user;
+	}
+}
